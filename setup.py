a9563e9111a53d023103d70569b94adf5fd7686d
ROSHNI DUBEY <97834953+Roshni011@users.noreply.github.com>
2024-06-11 07:28:52 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "transformers",
+    version = "1.2.0",
+    description = "Simple description",
+    packages = find_packages(),
+    install_requires = ['transformers']  # Example of external package
+)
